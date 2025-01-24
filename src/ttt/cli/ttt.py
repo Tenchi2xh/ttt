2f190879d9eb372ca4e849a99ee066176aa94812
Hamza Haiken <tenchi@team2xh.net>
2025-01-24 11:36:23 +0100

    Tweak click options

--- src/ttt/cli/ttt.py
+++ src/ttt/cli/ttt.py
@@ -1,6 +1,11 @@
 import click
 
 
-@click.group()
+context_settings = {
+    "help_option_names": ["-h", "--help"],
+    "max_content_width": 100,
+}
+
+@click.group(context_settings=context_settings)
 def ttt():
     pass
